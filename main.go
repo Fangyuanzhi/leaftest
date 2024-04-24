70044fde9a665cb71bae19652cc8eda86c575b14
haitao.fang <2388240829@qq.com>
2024-04-24 18:44:23 +0800

    1

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/binary"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:3563")
+	if err != nil {
+		panic(err)
+	}
+
+	// Hello 消息（JSON 格式）
+	// 对应游戏服务器 Hello 消息结构体
+	data := []byte(`{
+		"Hello": {
+			"Name": "leaf"
+		}
+	}`)
+
+	// len + data
+	m := make([]byte, 2+len(data))
+
+	// 默认使用大端序
+	binary.BigEndian.PutUint16(m, uint16(len(data)))
+
+	copy(m[2:], data)
+
+	// 发送消息
+	conn.Write(m)
+	
+	// 接受消息
+	buf := [512]byte{}
+	n, err := conn.Read(buf[:])
+	if err != nil {
+		fmt.Println("recv failed, err:", err)
+		return
+	}
+	fmt.Println(string(buf[2:n]))
+}
